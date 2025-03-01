e230f42e5de279920339a9825a17bbac16a06d67
Abir Shahadat Purab <a.s.purab0@gmail.com>
2025-03-01 10:45:45 +0600

    alert

---
+++ scripts/activity-log.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".taskButton").forEach(button => {
+    button.addEventListener("click", function() {
+        let taskTitle = this.parentElement.parentElement.querySelector("h2").innerText;
+        let logContainer = document.getElementById("activity-logs");
+
+        let now = new Date();
+        let timeString = now.toLocaleTimeString();
+
+        let logEntry = document.createElement("div");
+        logEntry.classList.add("bg-slate-200", "p-3", "rounded-lg", "m-2");
+        logEntry.innerHTML = `<p class = "text-xs text-center">You have Complete The Task <strong>${taskTitle}</strong> at ${timeString}</p>`;
+
+        logContainer.appendChild(logEntry);
+
+        alert(`Board Updated Successfully`);
+    })
+})
+
+document.getElementById("clear-history").addEventListener("click", function() {
+    let logContainer = document.getElementById("activity-logs");
+    logContainer.innerHTML = "<h4></h4>";
+})
